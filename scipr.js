a06874a0492e560aee1e8ac1eca0de63b6124ba6
VanshikaChikane <159158197+VanshikaChikane@users.noreply.github.com>
2024-05-16 11:59:47 +0530

    Add files via upload

---
+++ scipr.js
@@ -0,0 +1,40 @@
+
+const nav = document.querySelector('.nav-menu');
+const toggle = document.querySelector('.nav-toggle')
+toggle.onclick = function(){
+    nav.classList.toggle('show-nav')
+
+}
+
+//remove Navbar//
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    const navMenu = document.querySelector('.nav-menu')
+    navMenu.classList.remove('show-nav')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+//change Active color
+const linkColor = document.querySelectorAll('.nav-link')
+function colorLink(){
+    if(linkColor){
+        if(linkColor){
+            linkColor.forEach(L => L.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+}
+linkColor.forEach(L => L.addEventListener('click',colorLink))
+
+// chane header bg
+function scrollHeader(){
+    const scrollHeader = document.getElementById('header')
+    if(this.scrollY >= 200){
+        scrollHeader.classList.add('scroll-header')
+    }
+    else{
+        scrollHeader.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll',scrollHeader)
